b76a5114ae7fba0da546616275bb882a674df4e6
Troy Swanson <troy.evans@gm.com>
2021-02-18 16:58:32 -0600

    basic activities

--- app/src/main/java/com/example/pg_transparentfragment/MainActivity.kt
+++ app/src/main/java/com/example/pg_transparentfragment/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.pg_transparentfragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_goToActivityA.setOnClickListener {
+            Intent(this, ActivityA::class.java)
+                    .also { startActivity(it) }
+        }
     }
 }
