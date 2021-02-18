aadef3acdd646f75a1a9748ca988d3f3d6ea899b
Troy Swanson <troy.evans@gm.com>
2021-02-18 17:13:17 -0600

    put activityA logic into FragmentThatWasActivityA

--- app/src/main/java/com/example/pg_transparentfragment/ActivityA.kt
+++ app/src/main/java/com/example/pg_transparentfragment/ActivityA.kt
@@ -1,17 +1,15 @@
 package com.example.pg_transparentfragment
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_a.*
 
 class ActivityA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a)
-        btn_goToActivityB.setOnClickListener {
-            Intent(this, ActivityB::class.java)
-                .also { startActivity(it) }
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, FragmentThatWasActivityA())
+            commit()
         }
     }
 }
